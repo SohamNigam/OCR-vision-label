4eb20464b7b8a3558e0b7d16c9f242cd42fa0610
SohamNigam <n.soham@starkdigital.in>
2025-07-29 15:15:50 +0530

    alr

--- batch_run.py
+++
@@ -1,9 +0,0 @@
-from Ocr_pipeline import list_pdfs_in_bucket_folder, run_batch_vision_ocr, BUCKET_NAME, FOLDER_PREFIX, OUTPUT_VISION_PATH
-
-def main():
-    pdf_uris = list_pdfs_in_bucket_folder(BUCKET_NAME, FOLDER_PREFIX)
-    run_batch_vision_ocr(pdf_uris, OUTPUT_VISION_PATH)
-    print("Batch OCR submitted and completed.")
-
-if __name__ == "__main__":
-    main()
